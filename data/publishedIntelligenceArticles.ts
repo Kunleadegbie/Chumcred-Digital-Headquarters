aaf304be423d05923ef6982336c8e89d267f64f7
Kunleadegbie <87202636+Kunleadegbie@users.noreply.github.com>
2026-08-05 07:20:08 +0100

    Point published articles to Article 10 collection

--- data/publishedIntelligenceArticles.ts
+++ data/publishedIntelligenceArticles.ts
@@ -2,6 +2,6 @@ export {
   intelligenceArticles,
   getArticleBySlug,
   getRelatedArticles,
-} from "@/data/publishedIntelligenceArticlesV7";
+} from "@/data/publishedIntelligenceArticlesV8";
 
 export type { PublishedIntelligenceArticle } from "@/data/publishedIntelligenceArticlesBase";
